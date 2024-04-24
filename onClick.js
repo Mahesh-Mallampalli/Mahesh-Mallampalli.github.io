38d1a80d1d9e7cc888a32cbab6e50e926cee3ff2
mmaheswarnath <mmaheswarnath.chegg.com>
2024-04-24 17:43:38 +0530

    new commit-1

---
+++ onClick.js
@@ -0,0 +1,8 @@
+const onClick = () => {
+    //document.getElementById("side-nav").style.display = "flex";
+     document.getElementById("side-nav").style.width = "250px";
+}
+
+const onClickClose = () => {
+    document.getElementById("side-nav").style.width = "0px";
+}
